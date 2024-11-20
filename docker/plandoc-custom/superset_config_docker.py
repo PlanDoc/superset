330cfd9c11900f6a9416e967b7cf4cc4838e1277
izll <antalizn@gmail.com>
2024-11-20 17:36:51 +0100

    feat: modify TALISMAN_CONFIG

--- docker/plandoc-custom/superset_config_docker.py
+++ docker/plandoc-custom/superset_config_docker.py
@@ -38,3 +38,33 @@ LANGUAGES = {
     "en": {"flag": "us", "name": "English"},
     "hu": {"flag": "hu", "name": "Hungarian"}
 }
+
+TALISMAN_CONFIG = {
+    "content_security_policy": {
+        "base-uri": ["'self'"],
+        "default-src": ["'self'"],
+        "img-src": [
+            "'self'",
+            "blob:",
+            "data:",
+            "https://apachesuperset.gateway.scarf.sh",
+            "https://static.scarf.sh/",
+            # "https://avatars.slack-edge.com", # Uncomment when SLACK_ENABLE_AVATARS is True
+        ],
+        "worker-src": ["'self'", "blob:"],
+        "connect-src": [
+            "'self'",
+            "https://api.mapbox.com",
+            "https://events.mapbox.com",
+        ],
+        "object-src": "'none'",
+        "style-src": [
+            "'self'",
+            "'unsafe-inline'",
+        ],
+        "script-src": ["'self'", "'strict-dynamic'", "'unsafe-eval'"],
+    },
+    "content_security_policy_nonce_in": ["script-src"],
+    "force_https": False,
+    "session_cookie_secure": False,
+}
